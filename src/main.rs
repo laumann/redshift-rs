20f0b52bd0f270ee2bee4b5f7a336f0c11b85db0
Thomas Jespersen <tj@asseco.dk>
2016-05-18 14:07:52 +0200

    Implement interpolate_color_settings()
    Use an implementation of TransitionScheme

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,30 @@ use xcb::randr;
 
 mod solar;
 
+/**
+ * Constants
+ */
+const RANDR_MAJOR_VERSION: u32 = 1;
+const RANDR_MINOR_VERSION: u32 = 3;
+const NEUTRAL_TEMP:        f64 = 6500.0;
+const DEFAULT_DAY_TEMP:    f64 = 5500.0;
+const DEFAULT_NIGHT_TEMP:  f64 = 3500.0;
+const DEFAULT_BRIGHTNESS:  f64 = 1.0;
+const DEFAULT_GAMMA:       f64 = 1.0;
+
+/**
+ * Latitude and longitude location
+ */
 struct Location {
     lat: f64,
     lon: f64
 }
 
+/**
+ * A color setting
+ */
 struct ColorSetting {
-    temp: i32,
+    temp: f64,
     gamma: [f64; 3],
     brightness: f64,
 }
@@ -21,15 +38,30 @@ struct ColorSetting {
 impl ColorSetting {
     fn new() -> ColorSetting {
         ColorSetting {
-            temp: -1,
+            temp: -1.0,
             gamma: [std::f64::NAN, std::f64::NAN, std::f64::NAN],
             brightness: std::f64::NAN
         }
     }
 }
 
-const RANDR_MAJOR_VERSION: u32 = 1;
-const RANDR_MINOR_VERSION: u32 = 3;
+struct TransitionScheme {
+    high: f64,
+    low: f64,
+    day: ColorSetting,
+    night: ColorSetting
+}
+
+impl TransitionScheme {
+    fn new() -> TransitionScheme {
+        TransitionScheme {
+            high: 3.0,
+            low: solar::CIVIL_TWILIGHT_ELEV,
+            day: ColorSetting::new(),
+            night: ColorSetting::new()
+        }
+    }
+}
 
 struct Crtc {
     id: u32,
@@ -47,9 +79,38 @@ struct RandrState {
     crtcs: Vec<Crtc>
 }
 
+/**
+ * Ensure mid is at least lo, and at most up
+ */
+#[inline]
+fn clamp(lo: f64, mid: f64, up: f64) -> f64 {
+    if mid < lo {
+        lo
+    } else if mid > up {
+        up
+    } else {
+        mid
+    }
+}
+
+fn interpolate_color_settings(elevation: f64, trans: &TransitionScheme) -> ColorSetting {
+    let day = &trans.day;
+    let night = &trans.night;
+
+    let alpha = clamp(0.0, (trans.low - elevation) / (trans.low - trans.high), 1.0);
+
+    let mut res = ColorSetting::new();
+    res.temp = (1.0-alpha) * night.temp + alpha * day.temp;
+    res.brightness = (1.0-alpha) * night.brightness + alpha * day.brightness;
+    for i in 0..3 {
+        res.gamma[i] = (1.0-alpha) * night.gamma[i] + alpha*day.gamma[i];
+    }
+    res
+}
+
 fn main() {
     let mut randr_state = RandrState::init();
-    
+
     randr_state.query_version();
     randr_state.start();
 
@@ -61,16 +122,37 @@ fn main() {
     // transition: int
     // verbose: bool
 
+    /* Init transition scheme - all defaults for now */
+    let mut scheme = TransitionScheme::new();
+    scheme.day.temp = DEFAULT_DAY_TEMP;
+    scheme.night.temp = DEFAULT_NIGHT_TEMP;
+    if scheme.day.brightness.is_nan() {
+        scheme.day.brightness = DEFAULT_BRIGHTNESS;
+    }
+    if scheme.night.brightness.is_nan() {
+        scheme.night.brightness = DEFAULT_BRIGHTNESS;
+    }
+
+    if scheme.day.gamma[0].is_nan() {
+        for g in scheme.day.gamma.iter_mut() {
+            *g = DEFAULT_GAMMA;
+        }
+    }
+    if scheme.night.gamma[0].is_nan() {
+        for g in scheme.night.gamma.iter_mut() {
+            *g = DEFAULT_GAMMA;
+        }
+    }
+
     let mut now;
     loop {
         now = systemtime_get_time(); //::precise_time_s();
         println!("Adjusting at {:?}", now);
-        
+
         // Compute elevation
 
         // Interpolate color settings: ColorSetting
-        let mut color_setting = ColorSetting::new();
-        
+
         let lat = 55.0;
         let lon = 12.0;
 
@@ -80,12 +162,15 @@ fn main() {
         // Ongoing short transition?
 
         // Interpolate between 6500K and calculated temperature
+        let color_setting = interpolate_color_settings(elev, &scheme);
+        println!("Color temperature: {:?}K", color_setting.temp);
+        println!("Brightness: {:?}", color_setting.brightness);
 
         // randr_state.set_temperature(&color_setting)
-        
-        
+
+
         // Sleep for 5 seconds or 0.1 second
-        thread::sleep(std::time::Duration::from_secs(5));
+        thread::sleep(std::time::Duration::from_secs(1));
     }
 }
 
@@ -96,11 +181,11 @@ fn systemtime_get_time() -> f64 {
 
 
 /**
- * 
+ *
  */
 impl RandrState {
     fn set_temperature(&mut self, setting: &ColorSetting) {
-        
+
     }
 
     /**
@@ -109,7 +194,7 @@ impl RandrState {
     fn start(&mut self) {
         let setup = self.conn.get_setup();
         let screen = setup.roots().nth(self.screen_num as usize).unwrap();
-        
+
         /* Get list of CRTCs for the screen */
         let screen_resources = randr::get_screen_resources(&self.conn,
                                                            self.window_dummy).get_reply().unwrap();
@@ -139,8 +224,8 @@ impl RandrState {
         let window_dummy = {
             let setup = conn.get_setup();
             let screen = setup.roots().nth(screen_num as usize).unwrap();
-            let window_dummy = conn.generate_id();            
-        
+            let window_dummy = conn.generate_id();
+
             xcb::create_window(&conn, 0, window_dummy, screen.root(), 0, 0, 1,
                                1, 0, 0, 0, &[]);
             conn.flush();
